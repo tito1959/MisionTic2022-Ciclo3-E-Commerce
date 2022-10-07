f4dd4dfe74d631e635e750af8de508921efe73de
Sebastian Galindo <sebasti1959@hotmail.com>
2022-10-07 01:55:14 -0500

    Revert "agregando endpoints de la aplicaicon rest en el controlador y estableciendo propiedades de las entidades"
    This reverts commit d367bfa8a9cb2403d5455c6b760378a2b680065c.

--- src/main/java/co/edu/misiontic/sebas/commerce/controller/CommerceController.java
+++ src/main/java/co/edu/misiontic/sebas/commerce/controller/CommerceController.java
@@ -7,10 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +17,6 @@ import co.edu.misiontic.sebas.commerce.service.CategoryService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080")
 public class CommerceController {
 
     /*
@@ -51,13 +46,7 @@ public class CommerceController {
 
     // read all products
     @GetMapping("/products")
-    public ResponseEntity<List<ProductDto>> getProductsWithTags() {
-        return null;
-    }
-
-    // read product by id
-    @GetMapping("/products/{id}")
-    public ResponseEntity<ProductDto> getPorudct(@PathVariable String id) {
+    public List<Product> getProducts() {
         return null;
     }
 
--- src/main/java/co/edu/misiontic/sebas/commerce/model/dto/UserDto.java
+++ src/main/java/co/edu/misiontic/sebas/commerce/model/dto/UserDto.java
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 
     private Long id;
-    private boolean admin;
     private String firstName;
     private String lastName;
     private String urlImg;
